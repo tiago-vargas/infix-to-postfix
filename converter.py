cdae5cb5c731b79b0d5feabfe03e36b96c67580f
Viviane Mayra <vivi.mayra01@gmail.com>
2022-10-19 19:18:04 -0300

    feat: convert two additions

--- converter.py
+++ converter.py
@@ -2,40 +2,27 @@ def convert(input: str) -> str:
 	elements = input.split(' ')
 	# '2 + 5 + 10'
 	# ['2', '+', '5', '+', '10']
+	# '2 5 10 + +'
 
 	if len(elements) == 3:
 		operator = elements[1]
-		first_number = elements[0]
-		second_number = elements[2]
+		left_number = elements[0]
+		right_number = elements[2]
 
-		result = first_number + ' ' + second_number + ' ' + operator
+		result = left_number + ' ' + right_number + ' ' + operator
 
-		if elements[1] == '+':
-			return result
-		elif elements[1] == '-':
-			return result
-		elif elements[1] == '*':
-			return result
-		elif elements[1] == '/':
-			return result
+		return result
 
 	elif len(elements) == 5:
 		first_operator = elements[1]
 		first_number = elements[0]
 		second_number = elements[2]
 
+		result = first_number + ' ' + second_number + ' ' + first_operator
+
 		second_operator = elements[3]
 		third_number = elements[4]
 
-		result = first_number + ' ' + second_number + ' ' + third_number + ' ' + first_operator + ' ' + second_operator
+		result = result + ' ' + third_number + ' ' + second_operator
 
 		return result
-
-		# if elements[1] == '+':
-		# 	return result
-		# elif elements[1] == '-':
-		# 	return result
-		# elif elements[1] == '*':
-		# 	return result
-		# elif elements[1] == '/':
-		# 	return result
