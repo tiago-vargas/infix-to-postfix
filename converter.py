fd1f643079327d5a91f584a6785b1bcc16ae6af2
Tiago Vargas <tiago.ce10@outlook.com>
2022-10-19 10:01:47 -0300

    feat: convert single addition

---
+++ converter.py
@@ -0,0 +1,2 @@
+def convert(input: str) -> str:
+	return '2 5 +'
--- test_converter.py
+++ test_converter.py
@@ -1,1 +1,9 @@
 from converter import *
+
+
+def test_simple_addition():
+	input = "2 + 5"
+
+	output = convert(input)
+
+	assert output == '2 5 +'
