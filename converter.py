01a84ef53908b68c63bdadc85e5b754bbb5258f1
Tiago Vargas <tiago.ce10@outlook.com>
2022-10-24 12:00:06 -0300

    agora funciona, falta fazer merge

--- converter.py
+++ converter.py
@@ -4,19 +4,49 @@ def convert(input: str) -> str:
 	operators = ['+', '-', '*', '/']
 	precedence = {'+': 1, '-': 1, '*': 2, '/': 2}
 
-	result_as_stack = []
+	symbols = []
+	answer = []
 
-	result_as_stack.append(elements[0])
+	last_symbol = ''
 
 	for i in range(len(elements)):
-		if elements[i] in operators:
-			operator = elements[i]
-			number_after_operator = elements[i + 1]
+		current_element = elements[i]
+		is_number = current_element not in operators
+		if is_number and last_symbol in ['*', '/']:
+			continue
 
-			result_as_stack.append(number_after_operator)
-			result_as_stack.append(operator)
+		if is_number:
+			answer.append(current_element)
 
-	result = ' '.join(result_as_stack)
+		is_operator = current_element in operators
+		if is_operator:
+			operator = current_element
+			symbols.append(operator)
+
+			if operator in ['*', '/']:
+				next_number = elements[i + 1]
+				answer.append(next_number)
+
+				symbols.pop()
+				answer.append(operator)
+
+			if operator in ['+', '-']:
+				if last_symbol in ['+', '-']:
+					popped_operator = symbols.pop(-2)
+
+					answer.append(popped_operator)
+				elif len(symbols) > 1 and symbols[-2] in ['+', '-']:
+					popped_operator = symbols.pop(-2)
+
+					answer.append(popped_operator)
+
+			last_symbol = elements[i]
+
+
+
+	for symbol in symbols[::-1]:
+		answer.append(symbol)
+
+	result = ' '.join(answer)
 
 	return result
-
