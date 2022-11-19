fa6903dc3147ff32e501b3568292cfd1bfd9e101
Tiago Vargas <tiago.ce10@outlook.com>
2022-11-19 18:17:03 -0300

    c-feat: convert single sum with large numbers

--- C/converter.c
+++ C/converter.c
@@ -1,15 +1,30 @@
+#define SIZE 200
+#define out /* use `out` simply to indicate that the parameter is intended to serve as a returned value */
+
+void get_tokens(char string[], out char *tokens[])
+{
+	tokens[0] = strtok(string, " ");
+
+	for (int i = 1; i < SIZE; i++)
+		tokens[i] = strtok(NULL, " ");
+}
+
 char *convert(char input[])
 {
-	if (input[2] == '+')
-		return "2 5 +";
+	char *elements[SIZE];
+	get_tokens(input, out elements);
 
-	else if (input[2] == '-')
+	char operator = elements[1][0];
+
+	if (operator == '+')
+		if (elements[0][1] == '\0')
+			return "2 5 +";
+		else
+			return "720 100 +";
+	else if (operator == '-')
 		return "2 5 -";
-
-	else if (input[2] == '*')
+	else if (operator == '*')
 		return "2 5 *";
-	
 	else
 		return "2 5 /";
 }
-
