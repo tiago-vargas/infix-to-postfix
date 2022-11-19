7d624f4241aa27e19b586f7d3a583b5b4d9c87a5
Tiago Vargas <tiago.ce10@outlook.com>
2022-11-19 18:41:23 -0300

    c-feat: convert inputs with floats

--- C/converter.c
+++ C/converter.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #define SIZE 200
 #define out /* use `out` simply to indicate that the parameter is intended to serve as a returned value */
 
-void get_tokens(char string[], out char *tokens[])
+void tokenize(char string[], out char *tokens[])
 {
 	tokens[0] = strtok(string, " ");
 
@@ -12,19 +15,14 @@ void get_tokens(char string[], out char *tokens[])
 char *convert(char input[])
 {
 	char *elements[SIZE];
-	get_tokens(input, out elements);
+	tokenize(input, out elements);
 
-	char operator = elements[1][0];
+	char *operator = elements[1];
+	char *first_number = elements[0];
+	char *second_number = elements[2];
 
-	if (operator == '+')
-		if (elements[0][1] == '\0')
-			return "2 5 +";
-		else
-			return "720 100 +";
-	else if (operator == '-')
-		return "2 5 -";
-	else if (operator == '*')
-		return "2 5 *";
-	else
-		return "2 5 /";
+	char *result = malloc((sizeof (char)) * 1000);
+	sprintf(result, "%s %s %s", first_number, second_number, operator);
+
+	return result;
 }
